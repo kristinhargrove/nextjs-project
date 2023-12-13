5472b66ae9091eb5660e31ff2a9e485894742f89
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2023-12-13 12:02:42 -0500

    inserts navbar into the layout

--- app/clients/page.js
+++
@@ -1,9 +0,0 @@
-const Clients = () => {
-  return (
-    <div>
-      <h1 className="text-7xl">Clients</h1>
-    </div>
-  );
-};
-
-export default Clients;
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Next.js Project',
-  description: 'Next.js Example by Kristin Hargrove',
-}
+  title: "Next.js Project",
+  description: "Next.js Example by Kristin Hargrove",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className='text-2xl text-primary'>Navbar</nav>
-        {children}
-        </body>
+        <Navbar />
+        <main className="px-8 py-20 max-w-6 mx-auto">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
