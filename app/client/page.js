899f4734a2ac55e7df8100a542ce9d9b7d348f29
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2023-12-13 13:36:37 -0500

    adds counter to client side server example page

--- app/client/page.js
+++ app/client/page.js
@@ -1,7 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 const Client = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1 className="text-7xl">Client</h1>
+      <h1 className="text-5xl font-bold mb-8">{count}</h1>
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        INCREASE
+      </button>
     </div>
   );
 };
